cf9da3831f63d46c2f8486df4dc2bf681859f9f9
Chad Danford <chad.m.danford@gmail.com>
2016-01-28 18:13:01 +0100

    Redesign of the frontpage to my bosses liking - actually improved the flexbox a little

--- intro2-flexbox-test.php
+++ intro2-flexbox-test.php
@@ -1,6 +1,6 @@
 <section class="intro">
-	<div class="flex-row">
-		<div class="flex-col menuarea">
+	<div class="row">
+		<div class="col col--2-of-12 menu-placeholder">
 			<nav class="nav language">
 				<?php service_nav(); ?>
 			</nav>
@@ -13,27 +13,30 @@
 			</div>
 			<a href="javascript:void(0);" class="menu-activator unstuck">&#9776;</a>
 		</div>
-
-		<div class="flex-col flex-stretch">
-			<?php $museumImage = wp_get_attachment_url( get_post_thumbnail_id(589) ); ?>
-			<div class="intro-key" style="background:url(<?php echo $museumImage; ?>)center center;">
-				<a href="<?php echo get_page_link(589); ?>" class="full-area-link"></a>
+		<div class="col col--10-of-12">
+			<div class="flex-container">
+				<div class="flex-child">
+					<?php $museumImage = wp_get_attachment_url( get_post_thumbnail_id(589) ); ?>
+					<div class="intro-key" style="background:url(<?php echo $museumImage; ?>)center center;">
+						<a href="<?php echo get_page_link(589); ?>" class="full-area-link"></a>
+					</div>
+					<a href="<?php echo get_page_link(589); ?>" class="intro-link">Museum</a>
+				</div>
+				<div class="flex-child">
+					<?php $besuchImage = wp_get_attachment_url( get_post_thumbnail_id(297) ); ?>
+					<div class="intro-key" style="background:url(<?php echo $besuchImage; ?>)center center;">
+						<a href="<?php echo get_page_link(297); ?>" class="full-area-link"></a>
+					</div>
+					<a href="<?php echo get_page_link(297); ?>" class="intro-link">Ihr Besuch</a>
+				</div>
+				<div class="flex-child">
+					<?php $eventsImage = wp_get_attachment_url( get_post_thumbnail_id(612) ); ?>
+					<div class="intro-key" style="background:url(<?php echo $eventsImage; ?>)center center;">
+						<a href="<?php echo get_page_link(612); ?>" class="full-area-link"></a>
+					</div>
+					<a href="<?php echo get_page_link(612); ?>" class="intro-link">Veranstaltungen & <br>Restaurant</a>
+				</div>
 			</div>
-			<a href="<?php echo get_page_link(589); ?>" class="intro-link">Museum</a>
 		</div>
-		<div class="flex-col flex-stretch">
-			<?php $besuchImage = wp_get_attachment_url( get_post_thumbnail_id(297) ); ?>
-			<div class="intro-key" style="background:url(<?php echo $besuchImage; ?>)center center;">
-				<a href="<?php echo get_page_link(297); ?>" class="full-area-link"></a>
-			</div>
-			<a href="<?php echo get_page_link(297); ?>" class="intro-link">Ihr Besuch</a>
-		</div>
-		<div class="flex-col flex-stretch">
-			<?php $eventsImage = wp_get_attachment_url( get_post_thumbnail_id(612) ); ?>
-			<div class="intro-key" style="background:url(<?php echo $eventsImage; ?>)center center;">
-				<a href="<?php echo get_page_link(612); ?>" class="full-area-link"></a>
-			</div>
-			<a href="<?php echo get_page_link(612); ?>" class="intro-link">Veranstaltungen & <br>Restaurant</a>
-		</div>
-	</div> <!--end row -->
+	</div>
 </section>
--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 				<section>
 					<div class="row">
-						<div class="col col--3-of-12 menuarea-page">
+						<div class="col col--2-of-12 menuarea-page">
 							<nav class="nav language">
 								<?php service_nav(); ?>
 							</nav>
@@ -16,7 +16,7 @@
 							</div>
 							<a href="javascript:void(0);" class="menu-activator unstuck">&#9776;</a>
 						</div>
-						<div class="col col--9-of-12">
+						<div class="col col--10-of-12">
 							  <?php if (has_post_thumbnail() ): ?>
 							      <?php $featuredImage = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
 							      <div class="hero" style="background:url(<?php echo $featuredImage; ?>)bottom center;">
