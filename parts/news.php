f39d9bbf37a4a532f0ae3a4a3f7246c5d5a1d2cb
Chad Danford <chad.m.danford@gmail.com>
2016-02-04 17:04:10 +0100

    new news section with thumbnails and divistion floats

--- parts/news.php
+++ parts/news.php
@@ -6,19 +6,22 @@
 
 	<?php if ( $the_query->have_posts() ) : ?>
 		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<!-- <h3 class="news-date"><?php the_date( 'm/d/Y' ); ?> </h3> -->
-			<h3 class="news-title"><?php the_title(); ?></h3>
-			<?php if (has_post_thumbnail()): ?>
-				<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'medium' );?>
-				<div style="background:url('<?php echo $thumb['0'];?>')no-repeat center center;" class="news-thumbnail"></div>
-			<?php else: ?>
-			<?php endif; ?>
 
-			<p class="news-text"><?php the_excerpt(); ?></p>
-			<a href="<?php the_permalink(); ?>" class="news-link">mehr lesen</a>
-			<br class="clear">
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div class="left">
+				<?php if (has_post_thumbnail()): ?>
+					<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'small' );?>
+					<div style="background:url('<?php echo $thumb['0'];?>')no-repeat center center;" class="news-thumbnail"></div>
+				<?php else: ?>
+				<?php endif; ?>
+			</div>
+			<div class="right">
+				<h3 class="news-date"><?php the_date( 'm/d/Y' ); ?> </h3>
+				<h3 class="news-title"><?php the_title(); ?></h3>
+				<p class="news-text"><?php the_excerpt(); ?></p>
+				<a href="<?php the_permalink(); ?>" class="news-link">mehr lesen</a>
+				<br class="clear">
+			</div>
 		</article>
 			<?php endwhile; ?>
 	<?php endif; ?>
