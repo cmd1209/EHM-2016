e0731411398a8463c7161b6ecd53ef77ad76ae7b
Chad Danford <chad.m.danford@gmail.com>
2016-02-05 15:39:36 +0100

    backup before fixed header try

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@
   		$('.slidedown').toggleClass("active")
   		$('.stage').toggleClass("shoved")
 		});
-		
+
 		$(".owl-carousel").owlCarousel({
 			autoPlay : 10000,
 			navigation: false,
@@ -17,6 +17,7 @@
 			goToFirstSpeed : 3500,
 			singleItem : true,
 		});
+
 });
 
 })(jQuery, this);
