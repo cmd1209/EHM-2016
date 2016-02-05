2506c1aaab7bf42fa6e63f2e76b42ac5dc5c1af2
Chad Danford <chad.m.danford@gmail.com>
2016-02-05 17:16:59 +0100

    crazy fixed redesign

--- header.php
+++ header.php
@@ -31,29 +31,28 @@
 			</nav>
 		</div>
 		<div class="wrapper">
-			<div class="stage">  <!-- begin stage,-->
-				<div class="row">
-					<div class="col col--2-of-12 col--m-1-of-3">
-						<div class="sliding-header">
-							<div class="service-container">
-								<nav class="nav language">
-									<?php service_nav(); ?>
-								</nav>
-								<?php get_template_part('searchform'); ?>
-								<p class="opening-hours">Täglich geöffnet</br>(außer 24. Dezember)</p>
-								<p class="opening-hours">April – Oktober:</br>10.00 – 18.00 Uhr</p>
-								<p class="opening-hours">November-März:</br>10.00 – 17.00 Uhr</p>
-
-							</div>
+			<header>
+				<div class="header-position">
+					<div class="header-child">
+						<nav class="nav language">
+							<?php service_nav(); ?>
+						</nav>
+						<?php get_template_part('searchform'); ?>
+						<p class="opening-hours">Täglich geöffnet</br>(außer 24. Dezember)</p>
+						<p class="opening-hours">April – Oktober:</br>10.00 – 18.00 Uhr</p>
+						<p class="opening-hours">November-März:</br>10.00 – 17.00 Uhr</p>
+					</div>
+					<div class="header-child">
 						<div class="logo">
 							<a href="<?php echo home_url(); ?>">
 								<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
 								<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
 							</a>
 						</div>
-						<div class="service-container calendar">
-							<?php echo do_shortcode('[eo_calendar]'); ?>
-						</div>
-						<a href="javascript:void(0);" class="menu-activator">&#9776;</a>
 					</div>
-					</div> <!-- end of actuall header elements -->
+					<div class="header-child">
+						<?php echo do_shortcode('[eo_calendar]'); ?>
+					</div>
+					<a href="javascript:void(0);" class="menu-activator">&#9776;</a>
+				</div>
+			</header>
