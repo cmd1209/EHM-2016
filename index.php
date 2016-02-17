f2b2d594581d4ad8b019b0cf046368da67e973d1
Chad Danford <chad.m.danford@gmail.com>
2016-02-17 12:17:04 +0100

    language selector on index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <section id="content">
+<?php $currentlang = get_bloginfo('language'); if($currentlang=="de-DE"): ?>
   <div class="flex-container">
     <div class="flex-child">
       <?php $museumImage = wp_get_attachment_url( get_post_thumbnail_id(589) ); ?>
@@ -23,5 +24,30 @@
       </div>
     </div>
   </div>
+<?php else: ?>
+  <div class="flex-container">
+    <div class="flex-child">
+      <?php $museumImage = wp_get_attachment_url( get_post_thumbnail_id(814) ); ?>
+      <div class="intro-key museum" style="background:url(<?php echo $museumImage; ?>)center center;">
+        <a href="<?php echo get_page_link(814); ?>" class="intro-link"><?php echo get_the_title(814); ?></a>
+        <a href="<?php echo get_page_link(814); ?>" class="full-area-link"></a>
+      </div>
+    </div>
+    <div class="flex-child">
+      <?php $besuchImage = wp_get_attachment_url( get_post_thumbnail_id(410) ); ?>
+      <div class="intro-key besuch" style="background:url(<?php echo $besuchImage; ?>)center center;">
+        <a href="<?php echo get_page_link(410); ?>" class="intro-link"><?php echo get_the_title(410); ?></a>
+        <a href="<?php echo get_page_link(410); ?>" class="full-area-link"></a>
+      </div>
+    </div>
+    <div class="flex-child">
+      <?php $eventsImage = wp_get_attachment_url( get_post_thumbnail_id(816) ); ?>
+      <div class="intro-key veranstaltungen" style="background:url(<?php echo $eventsImage; ?>)center center;">
+        <a href="<?php echo get_page_link(816); ?>" class="intro-link"><?php echo get_the_title(816); ?></a>
+        <a href="<?php echo get_page_link(816); ?>" class="full-area-link"></a>
+      </div>
+    </div>
+  </div>
+<?php endif; ?>
   <?php get_footer(); ?>
 </section>
